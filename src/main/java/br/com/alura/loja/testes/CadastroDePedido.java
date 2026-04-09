5363902f27c1cea631974e631c677abb93c1a031
erick <erickbarretodasilva@gmail.com>
2026-04-09 18:16:58 -0300

    aula 2 - utilização de JPQL

--- src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -30,7 +30,12 @@ public class CadastroDePedido {
         PedidoDao pedidoDao = new PedidoDao(em);
         pedidoDao.cadastrar(pedido);
 
+
+
         em.getTransaction().commit();
+
+        BigDecimal totalVendido = pedidoDao.valorTotalVendido();
+        System.out.println("VALOR TOTAL: " + totalVendido);
     }
 
     private static void popularBancoDeDados() {
